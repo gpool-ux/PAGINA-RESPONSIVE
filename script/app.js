0fb7748fa19fb3119c986d5c6bbc907581d87103
Gianpool <gpoolcaicedo@gmail.com>
2020-01-28 10:16:10 -0500

    fin

---
+++ script/app.js
@@ -0,0 +1,32 @@
+const navSlide = () => {
+    const burguer = document.querySelector('.burguer');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+
+    burguer.addEventListener('click', ()=>{
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 0}'
+            }
+
+
+        });
+
+        //Burguer Animation
+        burguer.classList.toggle('toggle');
+        
+
+    });
+
+   
+}
+
+navSlide();
